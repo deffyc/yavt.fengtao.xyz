278cf798b14ab22250142a26da6e2125e1cc029f
FX <51405021+ratifx@users.noreply.github.com>
2020-04-07 10:12:57 +0800

    a.0.0.2

---
+++ questions.js
@@ -0,0 +1,176 @@
+questions = [
+
+    {
+        "question": "对于中国政府而言，比起中央统一指挥，增大地方政府自主权能更好服务当地人民",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0,
+            "envo": 1
+        }
+    },
+    {
+        "question": "秦朝以后大一统抑制了文化的创新",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 5,
+            "envo": 0
+        }
+    },
+    {
+        "question": "秦朝以后大一统抑制了文化的创新",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 5,
+            "envo": 2
+        }
+    },
+        {
+        "question": "国家给了我基本人权，主权高于人权（美国借塞尔维亚侵犯了科索沃人权对其进行军事干涉是错误，即使万人坑和种族屠杀真的存在）",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0,
+            "envo": 0
+        }
+    },
+        {
+        "question": "当前中国司法是有利于既得利益集团的",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 5,
+            "envo": 0
+        }
+    },
+        {
+        "question": "一个社会的政治、文化是受经济因素决定的",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0,
+            "envo": 0
+        }
+    },
+        {
+        "question": "在社会中保持等级制度，一部分人服从于另一部分人是必要的",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10,
+            "envo": 0
+        }
+    },
+        {
+        "question": "福利国家会削弱人的工作积极性",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0,
+            "envo": 0
+        }
+    },
+        {
+        "question": "通过税收再分配是对人财产权的侵犯",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -5,
+            "envo": 0
+        }
+    },
+        {
+        "question": "国有经济一定会变成少数权贵所有，不可能实现公有 （国有制距离公有制还有很长路要走，而且充满了困难）",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 5,
+            "envo": 0
+        }
+    },
+        {
+        "question": "现代社会中的很多弊端，都是由于我们背离了传统导致的。",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10,
+            "envo": 0
+        }
+    },
+        {
+        "question": "为了保护他们的人权，我们应该打开国门接纳难民",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 10,
+            "scty": 10,
+            "envo": 0
+        }
+    },
+        {
+        "question": "一切道德归根结底来自宗教",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10,
+            "envo": 0
+        }
+    },
+        {
+        "question": "在评判一个社会问题时，道德评判比法律评判更重要。",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -10,
+            "envo": 0
+        }
+    },
+        {
+        "question": "为了社会的和谐与有序，必须制定法律以外的礼法规范人的行为。",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10,
+            "envo": 0
+        }
+    },
+        {
+        "question": "在过去的七十年或一百余年中，中国的传统文化不断消逝，这造成了严重的社会问题。",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10,
+            "envo": 0
+        }
+    },
+        {
+        "question": "为尊者讳”的积极意义大于消极意义，我们不应该对领导人有任何不敬言行。领导犯错误的时候我们不应该直接批评，必须委婉地指出。",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -10,
+            "envo": -1
+        }
+    }
+
+
+
+];
